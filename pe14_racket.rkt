6076308db8c4a1b6ef6919b5370219c50ef03184
Michael Quigley <u0729216@utah.edu>
2015-12-30 15:11:56 -0700

    added racket solution

--- pe14_racket.rkt
+++ pe14_racket.rkt
@@ -1,9 +1,22 @@
 #lang racket
 
+;finds longest chain
 (define (countChain n)
     (if (= n 1) 0
      (if (= (modulo n 2) 0)
       (+ 1 (countChain (/ n 2)))
       (+ 1 (countChain (+ (* 3 n) 1))))))
 
-(write (countChain 8))
+;best is a list of of the following format: '(bestNum, bestNumCount)
+(define (collatz currNum maxNum best)
+    (if (> currNum maxNum)
+     best
+     (let ([tempCount (countChain currNum)])
+           (if (> tempCount (cadr best))
+            (collatz (+ currNum 1) maxNum (list currNum tempCount))
+            (collatz (+ currNum 1) maxNum best)))))
+
+(let ([result (collatz 1 1000000 '(0 0))])
+   (printf "Resulting number is ~s, max count is ~s\n" 
+           (car result)
+           (cadr result)))
