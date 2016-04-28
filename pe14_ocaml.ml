266a3ec094ebdff4a627ab58492c2d9902243e57
Michael Quigley <mquigley@cs.utah.edu>
2016-04-27 23:51:25 -0600

    Updated ocaml solution.

--- pe14_ocaml.ml
+++ pe14_ocaml.ml
@@ -4,13 +4,10 @@ let rec countN n cache =
         else if Hashtbl.mem cache n then
                 Hashtbl.find cache n
             else let cachedCount = 
-                if (n mod 2) = 0 then
-                  (1 + (countN (n / 2) cache))
-                else
-                  (1 + (countN ((3 * n) + 1) cache))
-                in(
-                    Hashtbl.add cache n cachedCount;
-                    cachedCount);;
+                match (n mod 2) with
+                | 0 -> (1 + (countN (n / 2) cache))
+                | _ -> (1 + (countN ((3 * n) + 1) cache))
+                in( Hashtbl.add cache n cachedCount; cachedCount);;
 
 (* returns the number that results in the highest number of iterations of the
  * collatz conjecture from min range to max range*)
@@ -19,10 +16,9 @@ let rec collatz (currNum, maxNum) cache (bestNum, bestNumCount) =
        (bestNum, bestNumCount)
     else
         let tempCount = (countN currNum cache) in
-           if tempCount >  bestNumCount then
-                collatz (currNum + 1, maxNum) cache (currNum, tempCount)
-           else
-                collatz (currNum + 1, maxNum) cache (bestNum, bestNumCount);;
+        collatz (currNum + 1, maxNum) cache (if (tempCount > bestNumCount) 
+                                             then (currNum, tempCount) 
+                                             else (bestNum, bestNumCount));;
 
 let startT = Sys.time();;
 let (resultNum, resultCount) = (collatz (1,1000000) (Hashtbl.create 1000) (0, 0));;
